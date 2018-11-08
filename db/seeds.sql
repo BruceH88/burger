bad679a468f3a6848892598345a0a9910c4feae8
Bruce Harms <bruceh_88@comcast.net>
2018-11-08 18:11:43 -0500

    functionality works

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+INSERT INTO burgers
+  (burger_name, devoured)
+VALUES
+  ("Big Mac", 0),
+  ("Whopper", 0),
+  ("Baconator Burger", 1),
+  ("WHoly Cow Burger", 1)
---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const exphbs = require("express-handlebars");
+const routes = require("./controllers/burgers_controller.js");
+
+const PORT = process.env.PORT || 8080;
+
+const app = express();
+
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(routes);
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+app.listen(PORT, function() {
+  console.log("Server listening on: http://localhost:" + PORT);
+});
